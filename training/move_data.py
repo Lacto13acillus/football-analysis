44bc30ee512ffffd20245e56974c3e3ff3340793
Lacto13acillus <dikad3557@gmail.com>
2026-02-17 22:11:50 +0700

    test

---
+++ training/move_data.py
@@ -0,0 +1,13 @@
+import shutil
+
+shutil.move('football-players-detection-20/train',
+            'football-players-detection-20/football-players-detection-20/train'
+            )
+
+shutil.move('football-players-detection-20/test',
+            'football-players-detection-20/football-players-detection-20/test'
+            )
+
+shutil.move('football-players-detection-20/valid',
+            'football-players-detection-20/football-players-detection-20/valid'
+            )
