ef3ea2ced1bd7acd9f2c74b2f28f301faaec1bb3
Lacto13acillus <dikad3557@gmail.com>
2026-02-17 20:31:51 +0700

    first commit

---
+++ yolo_inference.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8x')
+
+results = model.predict('', save=True)
+print = results([0])
+print = ('===================================')
+for box in results[0].boxes:
+    print(box)
