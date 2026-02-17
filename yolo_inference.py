100541612c5d3c31a50d90f53a7c41a8c2689ab8
Lacto13acillus <dikad3557@gmail.com>
2026-02-17 21:05:52 +0700

    test

--- yolo_inference.py
+++ yolo_inference.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8x')
 
 results = model.predict('./input_videos/football_analysis.mp4', save=True)
-print = results([0])
+print(results[0])
 print = ('===================================')
 for box in results[0].boxes:
     print(box)
