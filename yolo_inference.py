a341f01bd512ab7445cf9d8a86227870284a0bae
Lacto13acillus <dikad3557@gmail.com>
2026-02-17 21:10:44 +0700

    test

--- yolo_inference.py
+++ yolo_inference.py
@@ -4,6 +4,8 @@ model = YOLO('yolov8x')
 
 results = model.predict('./input_videos/football_analysis.mp4', save=True)
 print(results[0])
-print = ('===================================')
+separator = '==================================='
+print(separator)
+
 for box in results[0].boxes:
     print(box)
