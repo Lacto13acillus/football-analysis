b437c9501cde531a3bf3712812b488fc73e54b60
Lacto13acillus <dikad3557@gmail.com>
2026-02-18 16:11:10 +0700

    test

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO(' /home/dika/football-analysis/models/best.pt')
+model = YOLO('/home/dika/football-analysis/models/best.pt')
 
 results = model.predict('./input_videos/football_analysis.mp4', save=True)
 print(results[0])
