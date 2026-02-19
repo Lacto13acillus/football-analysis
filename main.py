fbe9db5dd38f9c7a0504e76d38743d73cdc6292b
Lacto13acillus <dikad3557@gmail.com>
2026-02-19 21:43:07 +0700

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.video_utils import read_video, save_video
 from trackers import Tracker
+from team_assigner.teamAssigner import TeamAssigner
 
 def main():
     #read_video
@@ -16,9 +17,22 @@ def main():
     ## Draw object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
 
     #save_video
-    save_video(output_video_frames, 'output_videos/football_analysis_output.avi')
+    save_video(output_video_frames, 'output_videos/football_analysis_output_color.avi')
 
 if __name__ == '__main__':
     main()
