37dc93037101dd774c54e278ab426c7058fe97c2
Lacto13acillus <dikad3557@gmail.com>
2026-02-24 22:22:34 +0700

    test

--- trackers/pass_detector.py
+++ trackers/pass_detector.py
@@ -5,15 +5,18 @@ import numpy as np
 
 class PassDetector:
     def __init__(self):
-        # DITURUNKAN menjadi 4 frame agar sentuhan bola yang sangat cepat tetap dihitung
-        self.pass_threshold_frames = 4  
+        self.pass_threshold_frames = 5  # Sedikit dinaikkan untuk mencegah sentuhan tak sengaja
         self.max_pass_distance = 500  
-        self.min_pass_distance = 40   
+        self.min_pass_distance = 90   # Ditingkatkan dari 40 ke 90 pixel agar jarak dekat tidak dihitung umpan
         
     def detect_passes(self, tracks, ball_possessions):
         passes = []
         last_possession = None
         possession_start_frame = None
+        
+        # FITUR BARU: Cooldown untuk mencegah perhitungan ganda (Flickering)
+        last_pass_frame = -999 
+        cooldown_frames = 15 # Jeda minimal 0.5 detik (@30fps) sebelum umpan baru bisa dicatat
         
         for frame_num in range(len(ball_possessions)):
             current_possession = ball_possessions[frame_num]
@@ -38,20 +41,22 @@ class PassDetector:
                         to_pos = get_center_of_bbox_bottom(to_player_data['bbox'])
                         distance = measure_distance(from_pos, to_pos)
                         
+                        # Validasi: Jaraknya harus cukup jauh DAN sudah melewati masa cooldown
                         if self.min_pass_distance < distance < self.max_pass_distance:
-                            
-                            pass_event = {
-                                'frame_start': possession_start_frame,
-                                'frame_end': frame_num,
-                                'from_player': last_possession,
-                                'to_player': current_possession,
-                                'distance': distance,
-                                'success': True, # Semuanya dianggap sukses karena ini drill kawan
-                                'from_pos': from_pos,
-                                'to_pos': to_pos
-                            }
-                            
-                            passes.append(pass_event)
+                            if (frame_num - last_pass_frame) > cooldown_frames:
+                                pass_event = {
+                                    'frame_start': possession_start_frame,
+                                    'frame_end': frame_num,
+                                    'from_player': last_possession,
+                                    'to_player': current_possession,
+                                    'distance': distance,
+                                    'success': True,
+                                    'from_pos': from_pos,
+                                    'to_pos': to_pos
+                                }
+                                
+                                passes.append(pass_event)
+                                last_pass_frame = frame_num # Catat frame umpan ini untuk cooldown berikutnya
                 
                 last_possession = current_possession
                 possession_start_frame = frame_num
