b86e7a7bdfff9044aebcf293d7bd164bb8b4ed76
Lacto13acillus <dikad3557@gmail.com>
2026-03-11 23:50:50 +0700

    test

--- training/get_dataset.py
+++ training/get_dataset.py
@@ -2,8 +2,9 @@ from roboflow import Roboflow
 
 from roboflow import Roboflow
 rf = Roboflow(api_key="xU5K9pp8upqFLYLfirA0")
-project = rf.workspace("lacto13acillus").project("football-drill")
+project = rf.workspace("lacto13acillus").project("penalty-kick")
 version = project.version(1)
 dataset = version.download("yolov8")
+                
                 
                 
