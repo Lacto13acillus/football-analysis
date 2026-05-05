61f463c6ba949f08a695ac29fe97abd74e9150e9
Lacto13acillus <dikad3557@gmail.com>
2026-05-05 21:50:04 +0700

    test

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('/home/dika/football-analysis/models/passing.pt')
 
-results = model.predict('./input_videos/passing.mp4', save=True)
+results = model.predict('./input_videos/passing_number.mp4', save=True)
 print(results[0])
 separator = '==================================='
 print(separator)
